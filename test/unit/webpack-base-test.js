bf86c07cad050a2fd392f59454005063434a8dc8
liyang <liyang@xiaobu121.com>
2021-06-10 13:31:18 +0800

    持续集成

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,8 +5,8 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
 
   it('entry', () => {
-    assert.ok(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
-    assert.ok(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
+    assert.ok(baseConfig.entry.index.indexOf('webpack-builder-demo/test/smoke/template/src/index/index.js') > -1, true);
+    assert.ok(baseConfig.entry.search.indexOf('webpack-builder-demo/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
  
